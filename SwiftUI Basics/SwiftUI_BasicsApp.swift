402cbab610918f1c275ec11b5f50c4c4ce3bc519
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-09-30 22:20:04 -0700

    Initial Commit

---
+++ SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Chris McAnulty on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
