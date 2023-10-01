5cad9bbadf442ca6775cb836e8bcf5b6af0586a0
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-09-30 22:30:51 -0700

    first commit

--- SwiftUI Basics/ContentView.swift
+++ SwiftUI Basics/ContentView.swift
@@ -1,21 +1,9 @@
-//
-//  ContentView.swift
-//  SwiftUI Basics
-//
-//  Created by Chris McAnulty on 2023-09-30.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("SwiftUI Basics")
     }
 }
 
