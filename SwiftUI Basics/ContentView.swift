23404382a58063075bcfa5959935b6164eae673c
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-09-30 22:39:38 -0700

    practice background color and text styling

--- SwiftUI Basics/ContentView.swift
+++ SwiftUI Basics/ContentView.swift
@@ -3,7 +3,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("SwiftUI Basics")
+        ZStack {
+            // Background color
+            LinearGradient(colors: [
+                .red, .purple, .green, .pink
+            ], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .opacity(0.8)
+            .ignoresSafeArea()
+            // Text
+            Text("SwiftUI Basics")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                .foregroundColor(.black)
+        }
     }
 }
 
